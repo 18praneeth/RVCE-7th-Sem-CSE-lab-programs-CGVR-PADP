af79f66fb04cb66868d573810edf8496169f2c0e
Praneeth <18praneeth@gmail.com>
2023-01-02 12:24:16 +0530

    updated rectangle spin program

--- CGVR/rectangle_spin.cpp
+++ CGVR/rectangle_spin.cpp
@@ -1,19 +1,19 @@
 #include <GL/glut.h>
 
-int id1,id2;
+int spinWin;
 
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glBegin(GL_POLYGON);
 	glColor3f(1, 0, 0);
-	glVertex2f(100, 100);
+	glVertex2f(-150, -150);
 	glColor3f(0, 1, 0);
-	glVertex2f(100, 400);
+	glVertex2f(-150, 150);
 	glColor3f(0, 0, 1);
-	glVertex2f(400, 400);
+	glVertex2f(150, 150);
 	glColor3f(1, 1, 0);
-	glVertex2f(400, 100);
+	glVertex2f(150, -150);
 	glEnd();
 
 	glFlush();
@@ -21,29 +21,28 @@ void display() {
 }
 
 void idle() {
-	glutSetWindow(id2);
-	glTranslatef(250, 250, 0);
+	glutSetWindow(spinWin);
 	glRotatef(.10, 0, 0, 1);
-	glTranslatef(-250, -250, 0);
 	glutPostRedisplay();
 }
 
 void myInit() {
 	glClearColor(1, 1, 1, 1);
-	gluOrtho2D(0, 500, 0, 500);
+	gluOrtho2D(-250, 250, -250, 250);
 }
 
 int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 100);
 
-	id1 = glutCreateWindow("Rect 1");
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Rect 1");
 	glutDisplayFunc(display);
 	myInit();
 
-	id2 = glutCreateWindow("Rect 2");
+	glutInitWindowPosition(600, 100);
+	spinWin = glutCreateWindow("Rect 2");
 	glutDisplayFunc(display);
 	glutIdleFunc(idle);
 	myInit();
