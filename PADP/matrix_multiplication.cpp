b6792551fe3ec326eb9d8d039d3b4a9857b98cc6
Sourav Kannantha B <souravkannanthab3219@gmail.com>
2023-02-13 01:38:50 +0530

    Update matrix_multiplication.cpp

--- PADP/matrix_multiplication.cpp
+++ PADP/matrix_multiplication.cpp
@@ -2,7 +2,7 @@
 #include<omp.h>
 #include<stdlib.h>
 
-#define DIM 1500
+#define DIM 1000
 
 int main() {
 	int *A = new int[DIM * DIM];
@@ -15,9 +15,13 @@ int main() {
 	for (int i = 0; i < DIM; i++)
 		for (int j = 0; j < DIM; j++)
 			for (int k = 0; k < DIM; k++)
-				// C[i][k] == C[DIM*i+k]
+				// C[i][k] == C[DIM*i + k]
 				C[DIM*i + k] += A[DIM*i + j] * B[DIM*j + k];
 
 	t = omp_get_wtime() - t;
 	printf("TIME: %lf", t);
+
+	delete[] A;
+	delete[] B;
+	delete[] C;
 }
